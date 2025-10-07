f618b464dc5fe3b6e063b536762ac85c7592915b
mauro bigo <maurobigo14@gmail.com>
2025-10-06 23:11:26 -0300

    repository

--- src/main/java/org/example/repository/ContenedorRepository.java
+++ src/main/java/org/example/repository/ContenedorRepository.java
@@ -1,7 +1,16 @@
 package org.example.repository;
 
+import org.example.model.Contenedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
-public class ContenedorRepository {
+public interface ContenedorRepository extends JpaRepository<Contenedor, Long> {
 
+    Optional<Contenedor> findByIdentificador(String identificador);
+    
+    List<Contenedor> findByEstado(String estado);
 }
+
